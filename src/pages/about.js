106d200c3b5e9c67519ebba1d19ae42b3d567ce9
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-03-13 18:08:05 +0000

    about content changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,9 +27,21 @@ const SecondPage = () => (
     {Banner(infoObj)}
     <div className="content">
       <p>Hey! I'm Cameron, a graduate in Mathematics, looking to get into web development.</p><br></br>
-      <p>I was introduced to programming when I was 16 in college, where I was taught visual basic. I did decide to do mathematics at university though as its a broader area and allowed me more career options, including programming.</p><br></br>
-      <p>During university, I did some programming in R, Matlab etc. Mainly mathematical based languages, so you could say I haven't stopped programming since I was 16, however not in depth.</p><br></br>
-      <p>After university, I decided I wanted a career as a developer and started learning in depth. My main areas of expertise are JS and React, although I have experience in projects using other technologies, such as node.js</p>
+      <p>
+        I was introduced to programming when I was 16, in college, where I was taught visual basic and introduced to some of the concepts related to the field.
+      </p>
+      <br></br>
+      <p>
+        I love solving problems and thinking analytically. For this reason, I studied mathematics at university where I sharpened my analytical
+        mindset as well as being introduced to multiple programming languages.
+        I did some programming in R, Matlab etc. Mainly mathematical based languages, 
+        solving high level problems whilst learning new technology and new techniques.
+      </p>
+      <br></br>
+      <p>
+        I started learning more in depth after university. My main areas of expertise are JS and React, although I have experience in projects using other technologies, such as node.js.
+        I am now looking for an opportunity to develop these skills in a professional environment and enhance my level of understanding.
+      </p>
       <br></br>
       <h3>Interests</h3>
       <ul className="interests">
